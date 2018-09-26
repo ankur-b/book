8289c184a463b099f73eeb335d6eaad70cb33f3a
ankur-b <barvankur29@outlook.com>
2018-09-26 16:44:45 +0530

    xyz

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template("signup.html")
+
+@app.route('/signin')
+def signin():
+   return render_template("signin.html")
+
+if __name__ == '__main__':
+   app.run(debug = True)
