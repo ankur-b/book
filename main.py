b1784d1044c473a8adfc0763f1a640487bfc57b8
ankur-b <barveankur29@outlook.com>
2018-09-29 09:18:12 +0530

    add admin page and user page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect,url_for,session
 import sqlite3
 
 app = Flask(__name__)
@@ -20,19 +20,46 @@ def index():
         lname = request.form['last_name']
         email = request.form['email']
         passw = request.form['password']
-        c.execute("INSERT INTO users VALUES(?,?,?,?)",(fname,lname,email,passw))
-        c.close()
-        conn.close()
-        return render_template('signin.html', error = None)
+        cpass = request.form['cpassword']
+        if passw == cpass:
+            c.execute("INSERT INTO users VALUES(?,?,?,?)",(fname,lname,email,passw))
+            conn.commit()
+            c.close()
+            conn.close()
+            return render_template('signin.html', error = None)
+        else:
+            c.close()
+            conn.close()
+            return render_template('signup.html', error = "Make Sure Your Password and Confirm is Equal")    
     c.close()
     conn.close()
-    return render_template('signup.html', error = error)
+    return render_template('signup.html')
 
-
-
-@app.route('/signin')
+@app.route('/signin' ,methods=['GET','POST'])
 def signin():
-    return render_template("signin.html")
+    conn = sqlite3.connect("Users.db")
+    c = conn.cursor()
+    error = None
+    if request.method == 'POST':
+        print("hello worldcsxn l;")
+        emai = request.form['email']
+        passc = request.form['password']
+        if emai == "ankur@ankurbarve.me" and passc == "abhi1234":
+            return redirect(url_for('admin'))
+        else:
+            return redirect(url_for('user'))
+    c.close()
+    conn.close()
+    return render_template('signin.html', error = error)
+
+@app.route('/user')
+def user():
+    return render_template('user.html',username="allu")
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html',username="admin")
+
 
 if __name__ == '__main__':
    app.run(debug = True)
